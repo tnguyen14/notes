a24fc6ebf9b673fe307159ad3b593a61492634f8
Tri Nguyen <tri@tridnguyen.com>
2016-07-05 15:51:49 +0000

    add basic styles

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ var previewButton = document.querySelector('.preview-button');
 var textarea = document.querySelector('.write-content textarea');
 var preview = document.querySelector('.preview-content .markdown-body');
 
-previewButton.addEventListener('click', function (e) {
-	// e.preventDefault();
+previewButton.addEventListener('click', function () {
 	var content = textarea.value;
 	preview.innerHTML = md.render(content);
 });
