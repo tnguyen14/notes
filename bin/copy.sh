134200e39ea2752de2234a5ac4dae142a95aa48c
Tri Nguyen <tri@tridnguyen.com>
2016-10-13 02:28:50 -0400

    make copy command posix compatible

--- bin/copy.sh
+++ bin/copy.sh
@@ -1,9 +1,14 @@
-assets=( 
-	"node_modules/github-markdown-css/github-markdown.css"
-	"node_modules/octicons/build/sprite.octicons.svg"
-	"node_modules/hint.css/hint.base.min.css"
-)
+# Do the following instead of an arry to be POSIX compliant
+# http://unix.stackexchange.com/questions/102891/posix-compliant-way-to-work-with-a-list-of-filenames-possibly-with-whitespace
 
-for file in "${assets[@]}"; do
+ASSETS="node_modules/github-markdown-css/github-markdown.css
+node_modules/octicons/build/sprite.octicons.svg
+node_modules/hint.css/hint.base.min.css"
+
+set -f; IFS='
+'                           # turn off variable value expansion except for splitting at newlines
+for file in $ASSETS; do
+	set +f; unset IFS       # restore globbing and field splitting at all whitespace
 	cp -vf "$file" public/
 done
+set +f; unset IFS           # do it again in case $ASSETS was empty
