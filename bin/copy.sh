25c17039ab355d9abb101f5dffca12ad97063dce
Tri Nguyen <tri@tridnguyen.com>
2016-10-18 00:40:35 -0400

    use primercss markdown

--- bin/copy.sh
+++ bin/copy.sh
@@ -1,9 +1,11 @@
 # Do the following instead of an arry to be POSIX compliant
 # http://unix.stackexchange.com/questions/102891/posix-compliant-way-to-work-with-a-list-of-filenames-possibly-with-whitespace
 
-ASSETS="node_modules/github-markdown-css/github-markdown.css
+ASSETS="
+node_modules/primer-markdown/build/build.css
 node_modules/octicons/build/sprite.octicons.svg
-node_modules/hint.css/hint.base.min.css"
+node_modules/hint.css/hint.base.min.css
+"
 
 set -f; IFS='
 '                           # turn off variable value expansion except for splitting at newlines
